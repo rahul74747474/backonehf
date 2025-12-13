f26579e0acaa9fa88707e5768fe2a147be6b6976
ziyamohammad <mohammad2311061@akgec.ac.in>
2025-12-14 02:47:56 +0530

    changed cors origin

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ import CookieParser from "cookie-parser"
 const app = express()
 
 app.use(cors({
-   origin:"http://localhost:3000",
+   origin:"https://prismportal.netlify.app",
    credentials:true
 }))
 
