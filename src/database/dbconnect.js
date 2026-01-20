8ae47c854953d67cfa1d9a0eb95424f7d71fcc10
rahul74747474 <ghibli141525@gmail.com>
2026-01-20 19:42:56 +0530

    Update dbconnect.js

--- src/database/dbconnect.js
+++ src/database/dbconnect.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose"
 
 const connectdb = async() =>{
    try {
-    const connectioninstance = await mongoose.connect(`${process.env.MONGODB_URI}/${process.env.MONGODB_NAME}`)
+    const connectioninstance = await mongoose.connect(`${process.env.MONGO_DB_URI}`)
     console.log(`Database Connected /n db host = ${connectioninstance.connection.host}`)
    } catch (error) {
     console.log("Connection Failed" ,error.message)
