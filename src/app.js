be99b0421d06b4912c434a949ae9b0bd6ca2ce91
rahul74747474 <ghibli141525@gmail.com>
2026-01-20 20:16:35 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -4,10 +4,8 @@ import cookieParser from "cookie-parser"
 
 const app = express()
 
-app.use(cors({
-   origin:"http://localhost:3000",
-   credentials:true
-}))
+app.use(cors({ origin: true, credentials: true }))
+
 
 
 
